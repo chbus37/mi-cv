00c04b3a20cb2f4a39e2427b103bf9c8a47f0966
Christian Bustos <christianbus37@gmail.com>
2023-12-05 09:08:31 -0300

    add media queries 1024px

--- script.js
+++ script.js
@@ -8,5 +8,13 @@
         $btnMenu.firstElementChild.classList.toggle("none");
         $btnMenu.lastElementChild.classList.toggle("none");
         $menu.classList.toggle("is-active");
+    });
+
+    d.addEventListener("click",e => {
+        if(!e.target.matches(".menu a")) return false;
+
+        $btnMenu.firstElementChild.classList.remove("none");
+        $btnMenu.lastElementChild.classList.add("none");
+        $menu.classList.remove("is-active");
     })
 })(document);
