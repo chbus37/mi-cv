af4622907eaadd89e9e25f595c5ab154aa2b905d
Christian Bustos <christianbus37@gmail.com>
2023-12-12 16:27:16 -0300

    add form with scripts

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // FUNCION ANONIMA AUTOEJECUTABLE
 
+// MENU
+
 ((d)=>{
     const $btnMenu = d.querySelector(".menu-btn"),
     $menu = d.querySelector(".menu");
@@ -18,3 +20,40 @@
         $menu.classList.remove("is-active");
     })
 })(document);
+
+// CONTACT FORM
+
+((d) => {
+    const $form = d.querySelector(".contact-form"),
+      $loader = d.querySelector(".contact-form-loader"),
+      $response = d.querySelector(".contact-form-response");
+  
+    $form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      $loader.classList.remove("none");
+      fetch("https://formsubmit.co/ajax/christianbus37@gmail.com", {
+        method: "POST",
+        body: new FormData(e.target),
+      })
+        .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+        .then((json) => {
+          console.log(json);
+          location.hash = "#gracias";
+          $form.reset();
+        })
+        .catch((err) => {
+          console.log(err);
+          let message =
+            err.statusText || "Ocurrió un error al enviar, intenta nuevamente";
+          $response.querySelector(
+            "h3"
+          ).innerHTML = `Error ${err.status}: ${message}`;
+        })
+        .finally(() => {
+          $loader.classList.add("none");
+          setTimeout(() => {
+            location.hash = "#close";
+          }, 3000);
+        });
+    });
+  })(document);
